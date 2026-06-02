9bcb58c97b8a359ad4e686745fdcc64da1991bbe
NinjaCheetah <58050615+NinjaCheetah@users.noreply.github.com>
2026-06-01 23:23:08 -0400

    Finished 7 day visiting chef view This functions the same as the iOS app, where there are left and right arrows to navigate through the days. Also now properly displays "No visiting chefs today" when there aren't any visiting chefs scheduled.

--- app/src/main/java/dev/ninjacheetah/tigerdine/components/DateFormatters.kt
+++ app/src/main/java/dev/ninjacheetah/tigerdine/components/DateFormatters.kt
@@ -1,6 +1,8 @@
 package dev.ninjacheetah.tigerdine.components
 
+import kotlinx.datetime.DayOfWeek
 import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.Month
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.number
 import kotlinx.datetime.toLocalDateTime
@@ -8,8 +10,40 @@ import kotlin.time.Instant
 
 fun Instant.formatTigerDine(): String {
     val zone = TimeZone.of("America/New_York")
-    val ldt = this.toLocalDateTime(zone)
-    return "%02d:%02d".format(ldt.hour, ldt.minute)
+    val localDateTime = this.toLocalDateTime(zone)
+    return "%02d:%02d".format(localDateTime.hour, localDateTime.minute)
+}
+
+fun Instant.formatVisitingChef(): String {
+    val timeZone = TimeZone.of("America/New_York")
+    val date = toLocalDateTime(timeZone).date
+
+    val weekday = when (date.dayOfWeek) {
+        DayOfWeek.MONDAY -> "Monday"
+        DayOfWeek.TUESDAY -> "Tuesday"
+        DayOfWeek.WEDNESDAY -> "Wednesday"
+        DayOfWeek.THURSDAY -> "Thursday"
+        DayOfWeek.FRIDAY -> "Friday"
+        DayOfWeek.SATURDAY -> "Saturday"
+        DayOfWeek.SUNDAY -> "Sunday"
+    }
+
+    val month = when (date.month) {
+        Month.JANUARY -> "Jan"
+        Month.FEBRUARY -> "Feb"
+        Month.MARCH -> "Mar"
+        Month.APRIL -> "Apr"
+        Month.MAY -> "May"
+        Month.JUNE -> "Jun"
+        Month.JULY -> "Jul"
+        Month.AUGUST -> "Aug"
+        Month.SEPTEMBER -> "Sep"
+        Month.OCTOBER -> "Oct"
+        Month.NOVEMBER -> "Nov"
+        Month.DECEMBER -> "Dec"
+    }
+
+    return "$weekday, $month ${date.day}"
 }
 
 fun LocalDateTime.toYyyyMmDd(): String =
