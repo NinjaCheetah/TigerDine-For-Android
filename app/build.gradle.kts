c08709e3dd9f732b8438923c3615ff8b2f0450af
NinjaCheetah <58050615+NinjaCheetah@users.noreply.github.com>
2026-07-05 00:30:18 -0400

    Version bump because I accidentally uploaded 108 to Play Console already

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "dev.ninjacheetah.tigerdine"
         minSdk = 26
         targetSdk = 37
-        versionCode = 108
+        versionCode = 109
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
