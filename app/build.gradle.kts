a037152dff2885d7a2743d74eaa619c46390e9a2
NinjaCheetah <58050615+NinjaCheetah@users.noreply.github.com>
2026-07-06 23:51:32 -0400

    Enable generating debug symbols for release builds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,10 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+
+            ndk {
+                debugSymbolLevel = "FULL"
+            }
         }
     }
     compileOptions {
