3726b632716a5bda99870d03bc7cd4b5a2afc27b
NinjaCheetah <58050615+NinjaCheetah@users.noreply.github.com>
2026-07-09 20:25:40 -0400

    Properly update icon to use orange background I had already *technically* updated it before, but the exported PNGs hadn't been regenerated and were still using the grey background.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "dev.ninjacheetah.tigerdine"
         minSdk = 26
         targetSdk = 37
-        versionCode = 112
+        versionCode = 113
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
