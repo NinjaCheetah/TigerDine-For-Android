0d6d16dc4081bbc3777d3dada8704137b2ad0af5
NinjaCheetah <58050615+NinjaCheetah@users.noreply.github.com>
2026-07-19 01:14:33 -0400

    Updated about screen, now has important links Also gave it a preview so it's easier to work on

--- app/src/main/java/dev/ninjacheetah/tigerdine/ui/AboutScreen.kt
+++ app/src/main/java/dev/ninjacheetah/tigerdine/ui/AboutScreen.kt
@@ -1,28 +1,43 @@
 package dev.ninjacheetah.tigerdine.ui
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SegmentedListItem
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import dev.ninjacheetah.tigerdine.BuildConfig
+import dev.ninjacheetah.tigerdine.R
 import dev.ninjacheetah.tigerdine.data.state.LocalTopBarStateUpdater
 import dev.ninjacheetah.tigerdine.data.state.TopBarState
 import dev.ninjacheetah.tigerdine.ui.navigation.Routes
+import dev.ninjacheetah.tigerdine.ui.theme.TigerDineTheme
 
 @ExperimentalMaterial3Api
 @ExperimentalMaterial3ExpressiveApi
@@ -30,6 +45,8 @@ import dev.ninjacheetah.tigerdine.ui.navigation.Routes
 fun AboutScreen(
     navController: NavHostController
 ) {
+    val uriHandler = LocalUriHandler.current
+
     val updateTopBar = LocalTopBarStateUpdater.current
     val navBackStackEntry by navController.currentBackStackEntryAsState()
 
@@ -44,6 +61,17 @@ fun AboutScreen(
         }
     }
 
+    AboutScreenContent(
+        onOpenUri = { uriHandler.openUri(it) }
+    )
+}
+
+@ExperimentalMaterial3Api
+@ExperimentalMaterial3ExpressiveApi
+@Composable
+fun AboutScreenContent(
+    onOpenUri: (String) -> Unit
+) {
     val versionCode: Int = BuildConfig.VERSION_CODE
     val versionName: String = BuildConfig.VERSION_NAME
 
@@ -52,25 +80,42 @@ fun AboutScreen(
         modifier = Modifier.fillMaxSize()
     ) {
         Column(
-            verticalArrangement = Arrangement.spacedBy(12.dp),
+            verticalArrangement = Arrangement.spacedBy(20.dp),
             modifier = Modifier
                 .verticalScroll(rememberScrollState())
                 .padding(16.dp)
         ) {
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(12.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.ic_launcher_playstore),
+                    contentDescription = "The TigerDine logo with a pawprint overlayed over a fork and knife",
+                    modifier = Modifier
+                        .size(128.dp)
+                        .clip(RoundedCornerShape(10.dp))
+                )
+
+                Column {
+                    Text(
+                        "TigerDine for Android",
+                        style = MaterialTheme.typography.headlineLarge,
+                        fontWeight = FontWeight.Bold
+                    )
+
+                    Text(
+                        "An unofficial RIT dining app",
+                        style = MaterialTheme.typography.titleMedium,
+                        fontWeight = FontWeight.SemiBold,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant
+                    )
+                }
+            }
+
             Column {
                 Text(
-                    "TigerDine for Android",
-                    style = MaterialTheme.typography.headlineLarge,
-                    fontWeight = FontWeight.Bold
-                )
-                Text(
-                    "An unofficial RIT dining app",
-                    style = MaterialTheme.typography.headlineSmall,
-                    fontWeight = FontWeight.SemiBold,
-                    color = MaterialTheme.colorScheme.onSurfaceVariant
-                )
-                Text(
-                    "Version $versionCode (${versionName})",
+                    "Version $versionName (${versionCode})",
                     style = MaterialTheme.typography.titleMedium,
                 )
                 Text(
@@ -79,13 +124,126 @@ fun AboutScreen(
                 )
             }
 
+            Column(
+                verticalArrangement = Arrangement.spacedBy(3.dp)
+            ) {
+                Text(
+                    "Links",
+                    fontWeight = FontWeight.SemiBold
+                )
+
+                SegmentedListItem(
+                    verticalAlignment = Alignment.CenterVertically,
+                    leadingContent = {
+                        Icon(
+                            painter = painterResource(R.drawable.code_24px),
+                            contentDescription = "Icon of a less than symbol followed by a greater than symbol",
+                        )
+                    },
+                    content = {
+                        Text("Source Code")
+                    },
+                    onClick = {
+                        onOpenUri("https://github.com/NinjaCheetah/TigerDine-for-Android")
+                    },
+                    shapes = ListItemDefaults.segmentedShapes(
+                        index = 0,
+                        count = 4
+                    ),
+                    colors = ListItemDefaults.colors(
+                        containerColor = MaterialTheme.colorScheme.surfaceContainer,
+                    )
+                )
+
+                SegmentedListItem(
+                    verticalAlignment = Alignment.CenterVertically,
+                    leadingContent = {
+                        Icon(
+                            painter = painterResource(R.drawable.restaurant_24px),
+                            contentDescription = "Icon of a fork and a knife",
+                        )
+                    },
+                    content = {
+                        Text("TigerCenter")
+                    },
+                    onClick = {
+                        onOpenUri("https://tigercenter.rit.edu")
+                    },
+                    shapes = ListItemDefaults.segmentedShapes(
+                        index = 1,
+                        count = 4
+                    ),
+                    colors = ListItemDefaults.colors(
+                        containerColor = MaterialTheme.colorScheme.surfaceContainer,
+                    )
+                )
+
+                SegmentedListItem(
+                    verticalAlignment = Alignment.CenterVertically,
+                    leadingContent = {
+                        Icon(
+                            painter = painterResource(R.drawable.map_24px),
+                            contentDescription = "Icon of a partially folded paper map",
+                        )
+                    },
+                    content = {
+                        Text("Official RIT Map")
+                    },
+                    onClick = {
+                        onOpenUri("https://maps.rit.edu")
+                    },
+                    shapes = ListItemDefaults.segmentedShapes(
+                        index = 2,
+                        count = 4
+                    ),
+                    colors = ListItemDefaults.colors(
+                        containerColor = MaterialTheme.colorScheme.surfaceContainer,
+                    )
+                )
+
+                SegmentedListItem(
+                    verticalAlignment = Alignment.CenterVertically,
+                    leadingContent = {
+                        Icon(
+                            painter = painterResource(R.drawable.menu_book_2_24px),
+                            contentDescription = "Icon of a menu book",
+                        )
+                    },
+                    content = {
+                        Text("FD MealPlanner")
+                    },
+                    onClick = {
+                        onOpenUri("https://fdmealplanner.com/")
+                    },
+                    shapes = ListItemDefaults.segmentedShapes(
+                        index = 3,
+                        count = 4
+                    ),
+                    colors = ListItemDefaults.colors(
+                        containerColor = MaterialTheme.colorScheme.surfaceContainer,
+                    )
+                )
+            }
+
             Column {
                 Text(
                     "This app is not affiliated, associated, authorized, endorsed by, or in " +
                             "any way officially connected with the Rochester Institute of " +
-                            "Technology. This app is student created and maintained."
+                            "Technology. This app is student created and maintained.",
+                    color = MaterialTheme.colorScheme.onSurfaceVariant
                 )
             }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3ExpressiveApi::class, ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun AboutScreenPreview() {
+    TigerDineTheme {
+        CompositionLocalProvider(LocalTopBarStateUpdater provides {}) {
+            AboutScreenContent(onOpenUri = {})
         }
     }
 }
